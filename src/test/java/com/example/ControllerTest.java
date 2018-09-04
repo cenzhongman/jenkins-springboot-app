a368c7ebe1e1e7ae4527da34ed969d4c2ff58989
cenzhongman <cenzhongman@163.com>
2018-09-04 23:23:21 +0800

    刪除junit中的其他依赖 增加測試輸出信息

--- src/test/java/com/example/ControllerTest.java
+++ src/test/java/com/example/ControllerTest.java
@@ -5,16 +5,19 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+
 /**
  * @author 岑忠满
  * @date 2018/9/4 16:42
  */
 public class ControllerTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Test
     public void hello() {
         try {
-            assertEquals("Hello World!", new Controller().hello());
+            assertEquals("Hello World!", new Controller().hello(), outContent.toString());
             System.out.println("Output is " + "Hello World!");
         } catch (Exception e) {
             fail("Output is not " + "Hello World!");

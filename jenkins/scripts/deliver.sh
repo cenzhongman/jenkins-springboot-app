e9bf3206313ff2315a0cb313673dc892bbbd40c6
cenzhongman <cenzhongman@163.com>
2018-09-04 23:43:19 +0800

    修改mvn命令

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-echo '正在打包应用'
-set -x
-mvn clean install
-set +x
-
 echo '获得项目名'
 set -x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
@@ -13,6 +8,11 @@ set +x
 echo '获得项目版本'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+set +x
+
+echo '正在打包应用'
+set -x
+mvn clean install
 set +x
 
 echo '正在运行应用'

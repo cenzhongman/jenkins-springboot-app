e003ab5a064a0931816cc0077f579d2b262b4e6a
cenzhongman <cenzhongman@163.com>
2018-09-05 14:16:31 +0800

    增加测试的输出文件

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-echo '获得项目名'
 set -x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
+echo '获得项目名'
 
-echo '获得项目版本'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
-#
-#echo '正在打包应用'
-#set -x
-#mvn clean install
-#set +x
+echo '获得项目版本'
 
 echo '正在运行应用'
 set -x

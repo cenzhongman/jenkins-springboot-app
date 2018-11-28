163b163809d8d12a3511b8dfd656c949d39f5f21
cenzhongman <cenzhongman@163.com>
2018-11-28 11:46:16 +0800

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,33 +1,18 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
 
-# 获得项目名
 set -x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
-echo $NAME
+echo '获得项目名'
 
-# 获得项目版本
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
-echo $VERSION
+echo '获得项目版本'
 
-# kill函数
-killNetstat(){
-    processId=`ps -ef | grep ${NAME}-${VERSION}.jar | grep java | awk '{print $2}'`
-    if ["$processId" = ""]
-    then
-        
-    else
-        kill -9 $processId
-}
+echo '正在运行应用'
+set -x
+
 
 cp target/*.jar /
-
-killNetstat
-
-source /etc/profile
-
-nohup java -jar *.jar &
-
-tail -f nohup.out
+java -jar target/${NAME}-${VERSION}.jar

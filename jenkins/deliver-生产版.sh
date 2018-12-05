e7ccef94ac304ddfc6dd6bd5b104366613f5bfce
cenzhongman <cenzhongman@163.com>
2018-12-05 10:14:20 +0800

    持续集成1.3

--- jenkins/deliver-生产版.sh
+++ jenkins/deliver-生产版.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -x
 # 拿到名字变量
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+set +x
 
+set -x
 #拿到版本变量
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+set +x
 
 # 杀掉历史进程
 ps -ef | grep target/${NAME}-${VERSION}.jar |grep -v grep| awk '{print $2}' | xargs kill -9
@@ -12,4 +16,4 @@ ps -ef | grep target/${NAME}-${VERSION}.jar |grep -v grep| awk '{print $2}' | xa
 cp target/*.jar /
 
 # 开启新的进程
-java -jar target/${NAME}-${VERSION}.jar
+nohup java -jar target/${NAME}-${VERSION}.jar &
